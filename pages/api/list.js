78646b2591b191b2f529da4449eb01f02a465b33
SharenfromHR <danieljerome97@hotmail.com>
2025-05-15 22:27:46 +0100

    Update list.js

--- pages/api/list.js
+++ pages/api/list.js
@@ -1,6 +1,6 @@
 
 export default async function handler(req, res) {
-  const token = 'patuhWCTHWBMPBxh.293931b1ea1ca53985b623c51eba6babfdae817cd183178a79e0e6677950f70b';
+  const token = process.env.AIRTABLE_TOKEN;
   const baseId = 'appMp0eQ6wPjDZ9y3';
   const table = 'Confessions';
 
