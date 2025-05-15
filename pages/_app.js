08f5782dc00e9f076fc6cf19f508a99a2f76de1a
SharenfromHR <danieljerome97@hotmail.com>
2025-05-15 19:28:15 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://www.chatbase.co/embed.min.js";
-    script.setAttribute("chatbotId", "d_poBWtVe0Att0VbQoi8Q"); // Replace this
+    script.setAttribute("chatbotId", "d_poBWtVe0Att0VbQoi8Q");
     script.setAttribute("domain", "www.chatbase.co");
     script.defer = true;
     document.body.appendChild(script);
