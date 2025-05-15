344c8b26dc178757335d32f04f0593925b15d283
SharenfromHR <danieljerome97@hotmail.com>
2025-05-15 19:20:03 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.chatbase.co/embed.min.js";
+    script.setAttribute("chatbotId", "d_poBWtVe0Att0VbQoi8Q"); // Replace this
+    script.setAttribute("domain", "www.chatbase.co");
+    script.defer = true;
+    document.body.appendChild(script);
+  }, []);
+
   return <Component {...pageProps} />
 }
