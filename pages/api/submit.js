ad30303fd694216e537941c57dc68ec29cd5a445
SharenfromHR <danieljerome97@hotmail.com>
2025-05-15 21:36:11 +0100

    Update submit.js

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { confession } = req.body;
-    const token = 'patuhWCTHWBMPBxh.293931b1ea1ca53985b623c51eba6babfdae817cd183178a79e0e6677950f70b';
+    const token = 'patuhWCTHWBMPBxh';
     const baseId = 'appMp0eQ6wPjDZ9y3';
     const table = 'Confessions';
 
