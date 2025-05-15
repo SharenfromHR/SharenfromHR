1a888ef622da95de04188066020aea74b951e65d
SharenfromHR <danieljerome97@hotmail.com>
2025-05-15 21:09:35 +0100

    Update list.js

--- pages/api/list.js
+++ pages/api/list.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   const token = 'patuhWCTHWBMPBxh.293931b1ea1ca53985b623c51eba6babfdae817cd183178a79e0e6677950f70b';
   const baseId = 'appMp0eQ6wPjDZ9y3';
-  const table = 'Confession';
+  const table = 'Confessions';
 
   const response = await fetch(`https://api.airtable.com/v0/${baseId}/${table}?sort[0][field]=Created&sort[0][direction]=desc`, {
     headers: {
